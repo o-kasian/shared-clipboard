436dbca54b291bc818ab03dc945669679bc9c44a
Oleh Kasian <okasianep@sofi.org>
2020-11-03 15:07:43 +0200

    headers

--- client/src/main/java/net/okasian/listener/ChannelConsumer.java
+++ client/src/main/java/net/okasian/listener/ChannelConsumer.java
@@ -2,9 +2,16 @@ package net.okasian.listener;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 @Service
 public class ChannelConsumer {
@@ -19,6 +26,16 @@ public class ChannelConsumer {
 
     private volatile long lastTime = System.currentTimeMillis();
     private RestTemplate restTemplate = new RestTemplate();
+
+    public ChannelConsumer() {
+        restTemplate.setInterceptors(Arrays.asList((request, body, execution) -> {
+            request.getHeaders().add("Accept", "*/*");
+            request.getHeaders().add("Accept-Encoding", "gzip deflate br");
+            request.getHeaders().add("Accept-Language", "en");
+            request.getHeaders().add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0");
+            return execution.execute(request, body);
+        }));
+    }
 
     @Autowired
     private ClipboardProducer clipboardProducer;
--- client/src/main/java/net/okasian/listener/ChannelProducer.java
+++ client/src/main/java/net/okasian/listener/ChannelProducer.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+
 @Service
 public class ChannelProducer {
 
@@ -14,6 +16,16 @@ public class ChannelProducer {
     private String clientId;
 
     private RestTemplate restTemplate = new RestTemplate();
+
+    public ChannelProducer() {
+        restTemplate.setInterceptors(Arrays.asList((request, body, execution) -> {
+            request.getHeaders().add("Accept", "*/*");
+            request.getHeaders().add("Accept-Encoding", "gzip deflate br");
+            request.getHeaders().add("Accept-Language", "en");
+            request.getHeaders().add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0");
+            return execution.execute(request, body);
+        }));
+    }
 
     public void produce(final String data) {
         System.out.println(data);
